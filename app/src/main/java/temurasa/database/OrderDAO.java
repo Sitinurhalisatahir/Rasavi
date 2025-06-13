1d8f01f97dcc90285534330ec583d5b2758e6db5
Siti Nur Halisa Tahir <nurhalisatahirsiti@gmail.com>
2025-06-13 13:39:34 +0800

    feat menambahkan kode pada MenuDao dan MenuController

--- app/src/main/java/temurasa/database/OrderDAO.java
+++ app/src/main/java/temurasa/database/OrderDAO.java
@@ -1,9 +1,5 @@
 package temurasa.database;
-<<<<<<< HEAD
-public class OrderDAO {
-=======
 
-public class OrderDao {
->>>>>>> 2f27675dbbd93fbe7764e68286aeaccec903c447
+public class OrderDAO {
     
 }
