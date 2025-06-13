75702a79bd206b622badf3562b82ac42b211cf25
Shabrinazahrahramadhani <shbrinaa23@gmail.com>
2025-06-13 15:06:43 +0800

    Feat: tambahkan kode order controller

--- app/src/main/java/temurasa/controllers/OrderController.java
+++ app/src/main/java/temurasa/controllers/OrderController.java
@@ -1,5 +1,30 @@
 package temurasa.controllers;
 
-public class OrderController {
-    
+import java.util.List;
+
+import temurasa.database.OrderDao;
+import temurasa.models.Order;
+import temurasa.models.OrderItem;
+
+public class OrderController implements IBaseController<Order> {
+    private final OrderDao orderDao = new OrderDao();
+
+    @Override
+    public boolean tambah(Order order) {
+        // insertOrder mengembalikan id, return true jika berhasil
+        return orderDao.insertOrder(order) > 0;
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return orderDao.getAllOrders();
+    }
+
+    public boolean tambahItemKeOrder(int orderId, List<OrderItem> items) {
+        return orderDao.insertOrderItems(orderId, items);
+    }
+
+    public int tambahOrderReturnId(Order order) {
+        return orderDao.insertOrder(order);
+    }
 }
